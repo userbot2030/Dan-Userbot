76dfba43d38d318276bc0a29a95f8d6008eecc29
IONBOT <113283570+ionmusic@users.noreply.github.com>
2023-02-02 03:44:29 +0700

    Add files via upload

---
+++ Kazu/__main__.py
@@ -0,0 +1,47 @@
+import importlib
+from pyrogram import idle
+from uvloop import install
+
+
+from PyroKar.modules import ALL_MODULES
+from PyroKar import BOTLOG_CHATID, LOGGER, LOOP, aiosession, app, bots, ids
+from PyroKar.modules.basic import join
+
+BOT_VER = "2.0.0"
+CMD_HANDLER = ["." "," "?" "!"]
+MSG_ON = """
+💢 **𝙿𝚈𝚁𝙾𝚉𝚄-𝚄𝚂𝙴𝚁𝙱𝙾𝚃 𝙰𝙺𝚃𝙸𝙵** 💢
+╼┅━━━━━━━━━━╍━━━━━━━━━━┅╾
+❍▹ **Usᴇʀʙᴏᴛ Vᴇʀsɪᴏɴ -** `{}`
+❍▹ **Kᴇᴛɪᴋ** `{}alive` **Uɴᴛᴜᴋ Mᴇɴɢᴇᴄᴇᴋ Bᴏᴛ**
+╼┅━━━━━━━━━━╍━━━━━━━━━━┅╾
+"""
+
+
+async def main():
+    await app.start()
+    print("LOG: Founded Bot token Booting..")
+    for all_module in ALL_MODULES:
+        importlib.import_module("Kazu.modules" + all_module)
+        print(f"Successfully Imported {all_module} ")
+    for bot in bots:
+        try:
+            await bot.start()
+            ex = await bot.get_me()
+            await join(bot)
+            try:
+                await bot.send_message(BOTLOG_CHATID, MSG_ON.format(BOT_VER, CMD_HANDLER))
+            except BaseException:
+                pass
+            print(f"Started as {ex.first_name} | {ex.id} ")
+            ids.append(ex.id)
+        except Exception as e:
+            print(f"{e}")
+    await idle()
+    await aiosession.close()
+
+
+if __name__ == "__main__":
+    LOGGER("Kazu").info("PyroZu-Userbot Telah Aktif")
+    install()
+    LOOP.run_until_complete(main())
