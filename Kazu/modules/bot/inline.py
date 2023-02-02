953b83c42c5d4f90da927674ee154474ad3a1080
IONBOT <113283570+ionmusic@users.noreply.github.com>
2023-02-02 08:58:59 +0700

    Update inline.py

--- Kazu/modules/bot/inline.py
+++ Kazu/modules/bot/inline.py
@@ -85,7 +85,7 @@ async def ping_function(message: Message, answers):
     duration = (end - start).microseconds / 1000
     msg = (
         f"<b>❃ 𝙿𝚈𝚁𝙾𝚉𝚄-𝚄𝚂𝙴𝚁𝙱𝙾𝚃 ❃</b>\n\n"
-        f"❃ Pɪɴɢ : `{ping}` ms\n"
+        f"❃ Pɪɴɢ : `{duration}` ms\n"
         f"❃ Uᴘᴛɪᴍᴇ - "
         f"`{uptime}` \n"
         f"**✦҈͜͡➳ Bʀᴀɴᴄʜ: {branch} \n\n"
