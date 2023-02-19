cfa0756b640acacb389eda93cb3f422c8aabb46d
IONBOT <113283570+ionmusic@users.noreply.github.com>
2023-02-20 04:43:03 +0700

    Update notes.py

--- Kazu/modules/notes.py
+++ Kazu/modules/notes.py
@@ -8,6 +8,7 @@ from Kazu.utils import *
 from Kazu import *
 from .help import add_command_help
 
+cmd = [",", ".", "?", "*", "!" "$",]
 
 @Client.on_message(filters.command("notes", cmd) & filters.me)
 async def list_notes(client, message):
