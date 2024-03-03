aef9c980d9fce7a43f519793be1d43111218e388
bebekk2024 <161502478+bebekk2024@users.noreply.github.com>
2024-03-04 04:23:47 +0700

    bot.py

--- Kazu/modules/bot/bot.py
+++ Kazu/modules/bot/bot.py
@@ -65,7 +65,7 @@ async def on_plug_in_cb(_, callback_query: CallbackQuery):
     this_command = f"──「 **Help For {str(modul_name).upper()}** 」──\n\n"
     for x in commands:
         this_command += f"  •  **Command:** `.{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
-    this_command += "© @Html12text"
+    this_command += "© Dan-Userbot-@Disney_storeDan"
     bttn = [
         [InlineKeyboardButton(text="Return", callback_data="reopen")],
     ]
