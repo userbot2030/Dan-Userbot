41356ad7c6a907f63356f7d6b3ab6940270b94a4
bebekk2024 <161502478+bebekk2024@users.noreply.github.com>
2024-03-04 04:39:57 +0700

    updater.py

--- Kazu/modules/updater.py
+++ Kazu/modules/updater.py
@@ -175,7 +175,7 @@ async def upstream(client: Client, message: Message):
             repo.__del__()
             return
         await status.edit(
-            "`[HEROKU]: Update Deploy PyroZu-Userbot Sedang Dalam Proses...`"
+            "`[HEROKU]: Update Deploy Dan-Userbot Sedang Dalam Proses...`"
         )
         ups_rem.fetch(ac_br)
         repo.git.reset("--hard", "FETCH_HEAD")
@@ -192,7 +192,7 @@ async def upstream(client: Client, message: Message):
         except GitCommandError:
             pass
         await status.edit(
-            "`PyroZu-Userbot Berhasil Diupdate! Userbot bisa di Gunakan Lagi.`"
+            "`Dan-Userbot Berhasil Diupdate! Userbot bisa di Gunakan Lagi.`"
         )
     else:
         try:
@@ -201,7 +201,7 @@ async def upstream(client: Client, message: Message):
             repo.git.reset("--hard", "FETCH_HEAD")
         await updateme_requirements()
         await status.edit(
-            "`PyroZu-Userbot Berhasil Diupdate! Userbot bisa di Gunakan Lagi.`",
+            "`Dan-Userbot Berhasil Diupdate! Userbot bisa di Gunakan Lagi.`",
         )
         args = [sys.executable, "-m", "Kazu"]
         execle(sys.executable, *args, environ)
