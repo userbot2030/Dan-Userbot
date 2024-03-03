be1bcdb57a83566483f8a4fbb92549c4284ff346
bebekk2024 <161502478+bebekk2024@users.noreply.github.com>
2024-03-04 05:22:07 +0700

    inline.py

--- Kazu/modules/bot/inline.py
+++ Kazu/modules/bot/inline.py
@@ -96,7 +96,7 @@ async def pingme(client: Client, message: Message):
 
 async def karman_function(message: Message, answers):
     msg = (
-        f"ᴅᴀɴ-𝚄𝚂𝙴𝚁𝙱𝙾𝚃 \n"
+        f"𝙳𝙰𝙽-𝚄𝚂𝙴𝚁𝙱𝙾𝚃 ⚡\n"
         "ㅤㅤsᴛᴀᴛᴜs : ᴘʀᴇᴍɪᴜᴍ ᴀᴋᴛɪғ \n"
         f"ㅤㅤㅤㅤᴍᴏᴅᴜʟᴇs:</b> <code>{len(modules)} Modules</code> \n"
         f"ㅤㅤㅤㅤʙᴏᴛ ᴠᴇʀsɪᴏɴ: {BOT_VER} \n"
