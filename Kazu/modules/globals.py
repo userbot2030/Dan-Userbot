80528ef24fa5401cef84e3f2ec93147e181bd50a
bebekk2024 <161502478+bebekk2024@users.noreply.github.com>
2024-03-04 05:04:38 +0700

    globals.py

--- Kazu/modules/globals.py
+++ Kazu/modules/globals.py
@@ -55,7 +55,7 @@ async def gban_user(client: Client, message: Message):
         return await Kazu.edit("**Gak Bisa Di Gban Kontol karena dia Yang Buat Aku 🗿**")
     if user_id in WHITELIST:
         return await Kazu.edit(
-            "**Kau Gak Bisa Kontol Gban Dia Karena Dia Adalah admin @Karc0de 😡**"
+            "**Kau Gak Bisa Kontol Gban Dia Karena Dia Adalah admin @mhmdwldnnnn 😡**"
         )
     if user_id:
         try:
