a0f92b8ae6209145e2602ed31e705a88d30fd078
bebekk2024 <161502478+bebekk2024@users.noreply.github.com>
2024-03-04 02:48:46 +0700

    help.py

--- Kazu/modules/help.py
+++ Kazu/modules/help.py
@@ -80,7 +80,7 @@ async def module_helper(client: Client, message: Message):
     elif not message.reply_to_message and len(cmd) == 1:
         ac = PrettyTable()
         ac.header = False
-        ac.title = "PyroZu-UserBot Plugins"
+        ac.title = "Dan-UserBot Plugins"
         ac.align = "l"
         for x in split_list(sorted(CMD_HELP.keys()), 2):
             ac.add_row([x[0], x[1] if len(x) >= 2 else None])
