fa7af73456d02f728362ee13cb1b0e1391bf894c
bebekk2024 <161502478+bebekk2024@users.noreply.github.com>
2024-04-06 06:22:37 +0700

    constants.py

--- Kazu/helpers/constants.py
+++ Kazu/helpers/constants.py
@@ -10,19 +10,19 @@
 class First:
     ALIVE = "`Still Working On Fire 🔥`"
     REPO = """
-**Hey**, __I am using__ ✨ **𝙿𝚈𝚁𝙾𝚉𝚄-𝚄𝚂𝙴𝚁𝙱𝙾𝚃** ✨
+**Hey**, __I am using__ ✨ **𝙳𝙰𝙽-𝚄𝚂𝙴𝚁𝙱𝙾𝚃** ✨
 
 ✣ **Userbot Version :** `{}`
-✣ **Group Support :** [Kazu Support](t.me/kazusupportgrp)
-✣ **Channel Kazu :** [Kazu Session](t.me/Html12text)
-✣ **Owner Repo :** [Kazu](t.me/kenapatagkazu)
-✣ **Repo :** [PyroZu-Userbot](https://github.com/ionmusic/PyroZu-Userbot)
+✣ **Group Support :** [Dan Support](t.me/musik_supportdan)
+✣ **Channel Dan :** [Dan Session](t.me/Disney_storeDan)
+✣ **Owner Repo :** [Dan](t.me/mhmdwldnnnn)
+✣ **Repo :** [Dan-Userbot](https://github.com/bebekk/PyroZu-Userbot)
 
 
 Powered by [PyroMan-Userbot](https://github.com/mrismanaziz/pyroman-userbot)"""
-    CREATOR = 'I was created by my master <a href="https://github.com/ionmusic/PyroZu-Userbot">Kazu</a> on a rainy day.'
+    CREATOR = 'I was created by my master <a href="https://github.com/bebekk/PyroZu-Userbot">Kazu</a> on a rainy day.'
     MSG_ON = """
-⚡ **PyroZu-Userbot Berhasil Di Aktifkan** ⚡
+⚡ **Dan-Userbot Berhasil Di Aktifkan** ⚡
 ━━
 ➠ **Userbot Version -** `{}`
 ➠ **Ketik** `{}zu` **untuk Mengecheck Bot**
